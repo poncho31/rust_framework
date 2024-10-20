9bb523e08b95627facb3f38cd28bac294f5fee7a
poncho31 <gilles.pinchart@gmail.com>
2024-10-20 13:12:02 +0200

    init : sql create  in same file

--- migrations/2024-10-19-175541_create_tables/up.sql
+++ migrations/2024-10-19-175541_create_tables/up.sql
@@ -1,3 +1,10 @@
+CREATE TABLE users (
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       username TEXT NOT NULL UNIQUE,
+                       email TEXT NOT NULL UNIQUE,
+                       password_hash TEXT NOT NULL,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 CREATE TABLE events (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         title TEXT NOT NULL,
--- migrations/2024-10-19-175541_create_users/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-                       id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       username TEXT NOT NULL UNIQUE,
-                       email TEXT NOT NULL UNIQUE,
-                       password_hash TEXT NOT NULL,
-                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
